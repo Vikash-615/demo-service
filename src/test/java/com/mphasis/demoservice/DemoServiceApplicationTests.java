e0cc38671a57cf0f0ebf6779dfec4455138f8b01
Vikash-615 <vikash.singh2@mphasis.com>
2023-01-17 21:49:18 +0530

    Project setup

---
+++ src/test/java/com/mphasis/demoservice/DemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mphasis.demoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
