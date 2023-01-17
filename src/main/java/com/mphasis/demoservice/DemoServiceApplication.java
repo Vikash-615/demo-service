2722bc8a4993450432d97078f3c9458397e3c1ee
Vikash-615 <vikash.singh2@mphasis.com>
2023-01-17 21:55:06 +0530

    Demo Api created

--- src/main/java/com/mphasis/demoservice/DemoServiceApplication.java
+++ src/main/java/com/mphasis/demoservice/DemoServiceApplication.java
@@ -2,12 +2,21 @@ package com.mphasis.demoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoServiceApplication.class, args);
 	}
+
+	@GetMapping
+	public String getName() {
+		return "Hello, User	";
+	}
+
 
 }
